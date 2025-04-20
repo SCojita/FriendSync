29aa265a4edfa657d9ca49fcee82982dfb55fbe1
1DAMSCojita <scojita20207@iesjoanramis.org>
2025-04-20 15:38:53 +0200

    Lista de asistentes gestionada.

---
+++ app/src/main/java/org/stefancojita/friendsync/Asistente.java
@@ -0,0 +1,19 @@
+package org.stefancojita.friendsync;
+
+public class Asistente {
+
+    private String id;
+    private String email;
+
+    public Asistente() {}
+
+    public Asistente(String id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public String getEmail() { return email; }
+
+}
+
---
+++ app/src/main/java/org/stefancojita/friendsync/AsistenteAdapter.java
@@ -0,0 +1,49 @@
+package org.stefancojita.friendsync;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AsistenteAdapter extends RecyclerView.Adapter<AsistenteAdapter.AsistenteViewHolder> {
+
+    private List<Asistente> listaAsistentes;
+
+    public AsistenteAdapter(List<Asistente> listaAsistentes) {
+        this.listaAsistentes = listaAsistentes;
+    }
+
+    @NonNull
+    @Override
+    public AsistenteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new AsistenteViewHolder(vista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AsistenteViewHolder holder, int position) {
+        Asistente asistente = listaAsistentes.get(position);
+        holder.textView.setText(asistente.getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaAsistentes.size();
+    }
+
+    public static class AsistenteViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public AsistenteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(android.R.id.text1);
+        }
+    }
+}
+
